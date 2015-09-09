2847e2bb50354d2a727036656640ef124dfe7eda
Tomáš Musil <tomik.musil@gmail.com>
2015-09-09 02:02:15 +0200

    buffered reading

--- prep_fast_align/src/main.rs
+++ prep_fast_align/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::io::prelude::*;
+use std::io::BufReader;
 use std::fs::File;
 
 fn main() {
@@ -13,18 +14,18 @@ fn work() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let ref filename1 = args[1];
     let ref filename2 = args[2];
-    let mut f1 = try!(File::open(filename1));
-    let mut f2 = try!(File::open(filename2));
-    let mut s1 = String::new();
-    let mut s2 = String::new();
-    try!(f1.read_to_string(&mut s1));
-    try!(f2.read_to_string(&mut s2));
+    let f1 = try!(File::open(filename1));
+    let f2 = try!(File::open(filename2));
+    let s1 = BufReader::new(f1);
+    let s2 = BufReader::new(f2);
     let mut l2 = s2.lines();
     for l in s1.lines() {
-        print!("{} ||| ", l);
-        println!("{}", match l2.next() {
+        print!("{} ||| ", try!(l));
+        println!("{}", try!(match l2.next() {
             Some(val) => val,
-            None => ""} );
+            None => unimplemented!()
+            })
+        );
     }
     Ok(())
 }
