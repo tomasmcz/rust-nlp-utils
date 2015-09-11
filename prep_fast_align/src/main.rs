a02654541717ac1754c42a7f2c2a04a6eb4f2b5b
Tomáš Musil <tomik.musil@gmail.com>
2015-09-11 15:54:52 +0200

    another version

--- prep_fast_align/src/main.rs
+++ prep_fast_align/src/main.rs
@@ -21,8 +21,9 @@ fn work() -> std::io::Result<()> {
     let f2 = try!(File::open(filename2));
     let s1 = BufReader::new(f1);
     let s2 = BufReader::new(f2);
-    for (l1, l2) in s1.lines().zip(s2.lines()) { 
-        println!("{} ||| {}", try!(l1), try!(l2));
+    let mut it = s1.lines().zip(s2.lines());
+    while let Some((Ok(l1), Ok(l2))) = it.next() { 
+        println!("{} ||| {}", l1, l2);
     }
     Ok(())
 }
